3f83debf4dca20b65f54227b84527df9fca36fa5
Debrian <debrianfigueroa@gmail.com>
2018-05-15 10:09:36 -0400

    testing using camera ond device

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../shared/security/auth.service';
 
 @Component({
@@ -7,8 +7,12 @@ import { AuthService } from '../shared/security/auth.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  @ViewChild('videoElement') videoElement: any;
+  video: any;
+
   elementType : 'url' | 'canvas' | 'img' = 'url';
   value : string = '';
+  acceptanceStatus: string = 'Not Accepted';
 
   constructor(private authService: AuthService) {
     //this.value = authService.getCurrentUserID();
@@ -22,6 +26,29 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.video = this.videoElement.nativeElement;
+  }
+
+
+  start() {
+    this.initCamera({ video: true, audio: false });
+  }
+   sound() {
+    this.initCamera({ video: true, audio: true });
+  }
+
+    initCamera(config:any) {
+    var browser = <any>navigator;
+
+    browser.getUserMedia = (browser.getUserMedia ||
+      browser.webkitGetUserMedia ||
+      browser.mozGetUserMedia ||
+      browser.msGetUserMedia);
+
+    browser.mediaDevices.getUserMedia(config).then(stream => {
+      this.video.src = window.URL.createObjectURL(stream);
+      this.video.play();
+    });
   }
 
 }
