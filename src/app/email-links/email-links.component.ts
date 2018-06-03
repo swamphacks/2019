be7d860938e30aec0163b0150e36e00766dd5646
Debrian <debrianfigueroa@gmail.com>
2018-06-03 09:56:50 -0700

    added resend email verification feature

--- src/app/email-links/email-links.component.ts
+++ src/app/email-links/email-links.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/security/auth.service';
 
 @Component({
   selector: 'app-email-links',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailLinksComponent implements OnInit {
 
-  constructor() { }
+  emailInput: string = '';
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  resendEmail(){
+    let currentUser = this.authService.getAuth().auth.currentUser;
+    if(currentUser){
+      if(!currentUser.emailVerified){
+        this.authService.verifyEmail(currentUser).then(() => {
+          console.log('successfully send email');
+        })
+        .catch((error) => console.log('verify email error:', error));
+      }else{
+        alert('Email already verified');
+      }
+    }else{
+      console.log('no user logged in');
+    }
   }
 
 }
