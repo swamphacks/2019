5fc5bfb76faf68e3f72147c15d61056ad1a82197
Debrian <debrianfigueroa@gmail.com>
2018-07-28 15:05:51 -0700

    removing files from version control

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCkPM5Ta8WgzxpOP6O9WuLWD6Irkd6GAPc",
-    authDomain: "swamphacks-applicants.firebaseapp.com",
-    databaseURL: "https://swamphacks-applicants.firebaseio.com",
-    projectId: "swamphacks-applicants",
-    storageBucket: "swamphacks-applicants.appspot.com",
-    messagingSenderId: "622854578748"
-};
