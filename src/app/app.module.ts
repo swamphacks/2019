02410c2d7af91ee2874904dca355563cc9b01e4a
Debrian <debrianfigueroa@gmail.com>
2018-09-30 13:56:32 -0400

    removed components that were not going to be used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,18 +16,12 @@ import {routerConfig} from "./router.config";
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { SafeUrlPipe } from './shared/security/safe-url.pipe';
 import {ReactiveFormsModule} from "@angular/forms";
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import {AuthService} from "./shared/security/auth.service";
 import {DatabaseService} from "./shared/security/database.service";
 import {HttpModule} from "@angular/http";
 import {AngularFireDatabaseModule} from "angularfire2/database";
 import {AngularFireAuthModule} from "angularfire2/auth";
 import { NgxQRCodeModule } from 'ngx-qrcode2';
-import { ProfileComponent } from './profile/profile.component';
-import { MealsComponent } from './meals/meals.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { EmailLinksComponent } from './email-links/email-links.component';
 import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 import { RainComponent } from './coming-soon/rain-component/rain.component';
 
@@ -39,12 +33,6 @@ import { MatSnackBarModule } from '@angular/material';
     HomeComponent,
     TopMenuComponent,
     SafeUrlPipe,
-    LoginComponent,
-    RegisterComponent,
-    ProfileComponent,
-    MealsComponent,
-    ForgotPasswordComponent,
-    EmailLinksComponent,
     ComingSoonComponent,
     RainComponent,
   ],
--- src/app/router.config.ts
+++ src/app/router.config.ts
@@ -1,11 +1,5 @@
 import {Route} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {ProfileComponent} from "./profile/profile.component";
-import {RegisterComponent} from "./register/register.component";
-import {MealsComponent} from "./meals/meals.component";
-import {ForgotPasswordComponent} from "./forgot-password/forgot-password.component";
-import {EmailLinksComponent} from "./email-links/email-links.component";
 import {ComingSoonComponent} from "./coming-soon/coming-soon.component";
 
 export const routerConfig : Route[] = [
@@ -13,10 +7,6 @@ export const routerConfig : Route[] = [
       path: '',
       component: HomeComponent,
       pathMatch: 'full'
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
     },
     {
       path: 'comingsoon',
@@ -27,37 +17,7 @@ export const routerConfig : Route[] = [
         component: HomeComponent
     },
     {
-        'path': 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'profile',
-        component: ProfileComponent
-    },
-    {
-        path: 'meals',
-        component: MealsComponent
-    },
-    {
-        path: 'forgotpassword',
-        component: ForgotPasswordComponent
-    },
-    {
-        path: 'emaillinks',
-        component: EmailLinksComponent
-    },
-    {
-        path: 'register',
-        component: RegisterComponent,
-        pathMatch: 'full'
-    },
-    {
         path: '**',
         redirectTo: 'home'
     }
-        // },
-        // {
-        //     path: '**',
-        //     redirectTo: 'home'
-        // }
 ];
